ceec0bfa21cabca232df48f55368bb89811f090e
JuanCoutinho <desenvolvedor.juan.business@gmail.com>
2025-12-22 22:44:50 -0300

    chore: update logo image format from PNG to JPG.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Menu, X } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.jpg';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
